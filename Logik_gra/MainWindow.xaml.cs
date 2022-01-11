598134ec3f1a6928d4df7cdfc82707c10ba87dec
Mirek Stejskal <Stejskal.Mirek@stud.vassboskovice.cz>
2022-01-11 11:36:31 +0100

    11.1.2021

--- Logik_gra/MainWindow.xaml.cs
+++ Logik_gra/MainWindow.xaml.cs
@@ -24,5 +24,50 @@ namespace Logik_gra
         {
             InitializeComponent();
         }
+
+        private void Ellipse_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            Ellipse ellipse = (Ellipse)sender;
+            
+            if (ellipse.Fill == Brushes.LightGray)
+            {
+                ellipse.Fill = Brushes.Red;
+            }
+            else if (ellipse.Fill == Brushes.Red)
+            {
+                ellipse.Fill = Brushes.Green;
+            }
+            else if (ellipse.Fill == Brushes.Green)
+            {
+                ellipse.Fill = Brushes.Blue;
+            }
+            else if (ellipse.Fill == Brushes.Blue)
+            {
+                ellipse.Fill = Brushes.Yellow;
+            }
+            else if (ellipse.Fill == Brushes.Yellow)
+            {
+                ellipse.Fill = Brushes.Orange;
+            }
+            else if (ellipse.Fill == Brushes.Orange)
+            {
+                ellipse.Fill = Brushes.Purple;
+            }
+            else if (ellipse.Fill == Brushes.Purple)
+            {
+                ellipse.Fill = Brushes.Red;
+            }
+        } 
+        private void NastavVariantu0 (bool aktivni)
+        {
+            if(aktivni)
+            {
+                Ellipse0_0.MouseDown += Ellipse_MouseDown;
+            }
+            else
+            {
+                Ellipse0_0.MouseDown -= Ellipse_MouseDown;
+            }
+        }
     }
 }
